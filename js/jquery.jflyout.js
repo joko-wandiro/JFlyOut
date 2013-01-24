bcb8986ea2f28abed23e491d726fe5d41196880c
Joko Wandiro <joko_wandiro@yahoo.com>
2013-01-24 18:21:32 +0700

    Fixing Script

--- js/jquery.jflyout.js
+++ js/jquery.jflyout.js
@@ -6,14 +6,53 @@ Free for Private and Commercial Project
 (function($){
 $.fn.JFlyOut= function(options){
 	JFlyOut= {
+		'click': function($obj){
+/*			console.log("Event Click");*/
+			$('.icon', $obj).live('click', function(){
+				$elem= $(this);
+				$elemParent= $(this).parent();
+				if( $elemParent.hasClass('active') ){
+					JFlyOut.closeIt($elemParent);
+				}else{
+					JFlyOut.openIt($elem);
+				}
+			})
+		},	
 		'hover': function($obj){
-			
+/*			console.log("Event Hover");*/
+			$('.icon', $obj).mouseenter( function(){
+/*				console.log("Event mouseenter");*/
+				JFlyOut.openIt($(this));
+			});
+			$obj.mouseleave( function(){
+/*				console.log("Event mouseleave");*/
+				JFlyOut.closeIt($(this));
+			});
 		},
-		'toggle': function($obj){
-			
+		'openIt': function($obj){
+			$elem= $obj.parent();
+			$('.flyout').removeClass('active');
+			$elem.addClass('active');
+			JFlyOut.allContainer($('.flyout').not('.active'));
+			position= this.getPosition($elem.first());
+			prev_position= $elem.css(position);
+			if( prev_position != "0px" ){
+				$elem.attr({ 'data-prev-position': prev_position });
+			}
+			new_attr= this.getCSSObject("0px", parseInt(settings['z-index']));
+			$elem.css(new_attr);
 		},
-		'error': function(){
-			alert("There's something error on JFlyOut");
+		'closeIt': function($elem){
+			$elem.removeClass('active');
+			position= this.getPosition($elem);
+			prev_position= $elem.attr('data-prev-position');
+			new_attr= this.getCSSObject(prev_position, 0);
+			$elem.css(new_attr);
+		},
+		'allContainer': function($elems){
+			$elems.each( function(){
+				JFlyOut.closeIt($(this));
+			})
 		},
 		getPosition: function($obj){
 			return $obj.parent().attr('data-position');
@@ -23,15 +62,20 @@ $.fn.JFlyOut= function(options){
 			attr[position]= prev_position;
 			attr['z-index']= zindex;
 			return attr;
+		},
+		'error': function(){
+			alert("There's something error on JFlyOut");
 		}		
 	}
 	var settings= {
 		'z-index': 1,
+		'event': 'hover',
 		'startTop': '0',
 		'margin': '10',
 		'position': 'left',
 		'delay': '1000',
-		'positions': ['left', 'right', 'top', 'bottom']
+		'positions': ['left', 'right', 'top', 'bottom'],
+		'iconWidth': 36
 	};
 	
 	settings= $.extend(settings, options);
@@ -51,68 +95,65 @@ $.fn.JFlyOut= function(options){
 	// Icon Width and Height
 	iconWidth= $('.icon').first().outerWidth();
 	iconHeight= $('.icon').first().outerHeight();
-	
 	// Initial Method for Flyout Menu
 	this.attr({ 'data-position': settings.position }); // Adding Attribute Position	
 	$menus= $('.flyout', this);
+	$('.flyout .icon span', this).css({ 'padding-left': settings.iconWidth, 'line-height': iconHeight + "px" });
 	$menus.each( function(i){
 		$elem= $(this);
+		$icon= $elem.find('.icon');
+		$iconSpan= $elem.find('.icon span');
+		iconWidth= $iconSpan.outerWidth() + settings.iconWidth;
 		elemWidth= $elem.outerWidth();
+		console.log($elem);
+		console.log(elemWidth + "->" + iconWidth);
 		if( i == 0 ){
 			getTop= settings.startTop;
 		}else{
-			getTop= parseInt(settings.startTop) + ( (parseInt(settings.margin) + iconHeight) * i );
+			iconFactor= ( position == "top" || position == "bottom" ) ? iconWidth : iconHeight;
+			getTop= parseInt(settings.startTop) + ( (parseInt(settings.margin) + iconFactor) * i );
 		}
 		position= JFlyOut.getPosition($elem);
-		getRight= elemWidth - iconWidth + 1;
-		content_margin= iconWidth-2;
 		$content_box= $elem.find('.content-box');
-		content_boxHeight= $content_box.outerHeight();
-		$icon= $elem.find('.icon');
+		content_boxWidth= $content_box.width();
+		content_boxHeight= $content_box.outerHeight();		
 		// Set Initial Style Based position Attribute
 		attr= new Object;
+		icon_attr= new Object;
+		icon_attr['width']= iconWidth;
+		border_disable= "border-"+position;
+		icon_attr[border_disable]= "none";
 		if( position == "top" || position == "bottom" ){
 			set_position= -content_boxHeight + 'px';
 			attr.left= getTop + 'px';
 			if( position == "top" ){ $elem.append($icon); }			
-		}else{
-			icon_attr= new Object;
+		}else{			
 			pos_margin= ( position == "left" ) ? 'right' : 'left';
-			icon_attr['float']= pos_margin;
-			$icon.css(icon_attr);
+			icon_attr['float']= pos_margin;			
 			content_box_attr= new Object;
 			content_box_attr_margin= "margin-"+pos_margin;
-			content_box_attr[content_box_attr_margin]= content_margin + 'px';			
+			content_box_attr[content_box_attr_margin]= iconWidth + 'px';
 			$content_box.css(content_box_attr);
-			set_position= -getRight + 'px';
+/*			set_position= -elemWidth + 'px';*/
+			set_position= -elemWidth + 'px';
 			attr.top= getTop + 'px';
 		}
+		$icon.css(icon_attr);
 		attr[position]= set_position;
-		$elem.css(attr);
+/*		console.log(attr);*/
+		$elem.css(attr).attr({ 'data-prev-position': set_position });
 /*		console.log(attr);*/
 	});
-		
-	$('.icon', $menus).mouseenter( function(){
-		$elem= $(this).parent();
-		position= JFlyOut.getPosition($elem.first());
-		prev_position= $elem.css(position);
-		if( prev_position != "0px" ){
-			$elem.attr({ 'data-prev-position': prev_position });
-		}
-/*		console.log(prev_position + "->" + position);*/
-		new_attr= JFlyOut.getCSSObject("0px", parseInt(settings['z-index']));
-		$elem.css(new_attr);
-/*		console.log(new_attr);*/
-	});
 	
-	$menus.mouseleave( function(){
-		$elem= $(this);
-		position= JFlyOut.getPosition($elem);
-		prev_position= $elem.attr('data-prev-position');
-/*		console.log(position + "->" + prev_position);*/
-		new_attr= JFlyOut.getCSSObject(prev_position, 0);
-		$elem.css(new_attr);
-/*		console.log(new_attr);*/
-	});
+	// Trigger Event	
+	JFlyOut[settings.event]($menus);
 };
+
+$(document).ready( function(){
+/*	$('body').click( function(e){
+		if( e.target.tagName == "BODY" ){
+			JFlyOut.error();
+		}
+	});*/
+})
 })(jQuery);
